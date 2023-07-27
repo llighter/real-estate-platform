7b624ff3a3d9c1ca7e2e29e2e33ef6b0a758d8da
Park, Yunha <hadbsgk@gmail.com>
2023-07-27 23:30:09 +0900

    실행 시 기본 데이터 적재 추가

--- estate-service/src/main/java/com/llighter/estateservice/EstateServiceApplication.java
+++ estate-service/src/main/java/com/llighter/estateservice/EstateServiceApplication.java
@@ -1,13 +1,27 @@
 package com.llighter.estateservice;
 
+import com.llighter.estateservice.model.Customer;
+import com.llighter.estateservice.model.Items;
+import com.llighter.estateservice.repository.CustomerRepository;
+import com.llighter.estateservice.repository.ItemsRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EstateServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstateServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(CustomerRepository customers, ItemsRepository items) {
+		return args -> {
+			items.save(new Items("아파트", 100000000, "001", "001", "001", "001", "001"));
+			customers.save(new Customer("John", "1990-01-01", "1234567890"));
+		};
 	}
 
 }
